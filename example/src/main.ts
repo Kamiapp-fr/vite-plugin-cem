af2c1df07220b17c1ee188c492c285e7982cbe8b
Emilien Leroy <emilien1005@hotmail.fr>
2022-04-28 13:57:13 +0200

    ✨ Add api-viewer element into the example project

--- example/src/main.ts
+++ example/src/main.ts
@@ -1,2 +1,3 @@
+import 'api-viewer-element';
 import './style.css'
 import './title-element'
--- example/src/title-element.ts
+++ example/src/title-element.ts
@@ -1,14 +1,20 @@
 import { html, LitElement } from "lit";
 import { customElement, property } from 'lit/decorators.js'
 
+/**
+ * A title element.
+ *
+ * @element title-element
+ *
+ */
 @customElement('title-element')
 export class TitleElement extends LitElement {
   @property()
-  private name?: string;
+  public value: string = 'Title';
 
   render() {
     return html`
-      <h1>${this.name || ''}</h1>
+      <h1>${this.value || ''}</h1>
     `
   }
 }
