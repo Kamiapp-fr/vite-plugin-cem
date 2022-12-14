637f74e81a64df5a5740a5ee10a596aba144a7e3
Emilien Leroy <emilien1005@hotmail.fr>
2022-12-14 07:29:50 +0100

    🚨 Fix linter for tests

--- test/create.test.ts
+++ test/create.test.ts
@@ -5,16 +5,16 @@ const litElement = './example/lit-ts/src/my-element.ts';
 
 describe('#create', () => {
   describe('#createModule', () => {
-    it('should throw an error if component path does not exist', () => {    
-      expect(() => createModule('./example/fake/src/fake-element.ts')).to.throw()
+    it('should throw an error if component path does not exist', () => {
+      expect(() => createModule('./example/fake/src/fake-element.ts')).to.throw();
     });
 
     it('should create a module for a web component', () => {
       const module = createModule(litElement);
       const identifiers = Object.fromEntries(module.identifiers);
-      
+
       expect(identifiers['my-element']).to.equal('my-element');
-      expect(identifiers['lit']).to.equal('lit');
+      expect(identifiers.lit).to.equal('lit');
     });
   });
-})
+});
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
-import { defineConfig } from 'vitest/config'
+import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
     globals: true,
     coverage: {
-        provider: 'istanbul',
+      provider: 'istanbul',
     },
   },
-})
+});
